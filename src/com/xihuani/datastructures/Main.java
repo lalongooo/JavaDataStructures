79368f353b0e68b8156b04e446f3a912b125e890
Jorge Eduardo <hdez.jeduardo@gmail.com>
2014-06-21 23:16:10 -0500

    First commit

---
+++ src/com/xihuani/datastructures/Main.java
@@ -0,0 +1,39 @@
+package com.xihuani.datastructures;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Stack s = new Stack(5);
+		
+		s.push(1);
+		System.out.println(s.peek());
+		s.push(5);
+		System.out.println(s.peek());
+		s.push(3);
+		System.out.println(s.peek());
+		s.push(4);
+		System.out.println(s.peek());
+		s.push(2);
+		System.out.println(s.peek());
+
+		System.out.println("PUSH FINISHED!");
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+
+	}
+
+}
---
+++ src/com/xihuani/datastructures/Stack.java
@@ -0,0 +1,53 @@
+package com.xihuani.datastructures;
+
+public class Stack {
+	
+	private int size;
+	private int [] stackArray;
+	private int top = -1;
+	
+	Stack(int size) throws IllegalArgumentException {
+		if(size < 1){
+			throw new IllegalArgumentException("The size of the stack must be greater than zero.");
+		}
+		this.size = size;
+		stackArray = new int [size];		
+	}
+	
+	public int push(int newItem) {
+		if(top+1 < size){
+			top++;
+			stackArray[top] = newItem;
+			return top;
+		}else{
+			System.out.println("The stack is full.");	
+			return -1;
+		}
+	}
+	
+	public int pop(){
+		if(top > -1){
+			int oldItem = stackArray[top];
+			stackArray[top] = -1;
+			top--;
+			return oldItem;
+		}else{
+			System.out.println("The stack is empty.");
+			return -1;
+		}
+	}
+	
+	public int peek(){
+		if(top > -1){
+			return stackArray[top];
+		}else{
+			System.out.println("The stack is empty.");
+			return -1;
+		}
+	}
+	
+	public int size(){
+		return size;
+	}
+
+}
